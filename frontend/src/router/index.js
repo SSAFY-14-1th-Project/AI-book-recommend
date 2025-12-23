d298beb46ac770f42d813ac7f342f9b4fb655e1d
wl990@naver.com <wl990@naver.com>
2025-12-24 02:09:28 +0900

    feat : 로그인/로그아웃/회원가입 기능 연결 완료

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -17,7 +17,11 @@ const router = createRouter({
           path: 'login',
           name: 'login',
           component: () => import('@/views/LoginView.vue'),
-          
+        },
+        {
+          path: 'signup',
+          name: 'signup',
+          component: () => import('@/views/SignupView.vue'),
         },
       ],
     },
